cb2c2a032167b2a51046cdb908d350339fb1968a
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-01-27 11:56:56 +0530

    Create ubuntu-setup.sh

---
+++ ubuntu-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install geary mail client
+sudp apt-get install geary
+
+# install gradle
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+sudo apt-get install gradle-ppa
