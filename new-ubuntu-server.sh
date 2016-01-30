ab283847e4ec84fe42e26379a125954e364aa807
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-01-30 13:17:30 +0530

    Update new-ubuntu-server.sh

--- new-ubuntu-server.sh
+++ new-ubuntu-server.sh
@@ -8,10 +8,10 @@ sudo add-apt-repository -y ppa:cwchien/gradle
 sudo apt-get update
 sudo apt-get install -y gradle-ppa
 
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-set-default
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
 
 sudo apt-get install -y subversion
 sudo apt-get install -y git
