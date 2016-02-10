4d877bf94a5897515a7f25684c80d0ca18f9b4ca
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-02-10 23:58:49 +0530

    Update new-ubuntu-server.sh

--- new-ubuntu-server.sh
+++ new-ubuntu-server.sh
@@ -16,32 +16,34 @@ sudo apt-get install -y oracle-java8-set-default
 sudo apt-get install -y subversion
 sudo apt-get install -y git
 
+sudo mkdir -p /ebs/apps
+sudo chmod -R 777 /ebs/apps
 
 # scala..
 cd /ebs/apps/
-apt-get remove scala-library scala
-wget http://www.scala-lang.org/files/archive/scala-2.11.7.deb
-dpkg -i scala-2.11.7.deb
-apt-get update
-apt-get install scala
+sudo apt-get remove scala-library scala
+sudo wget http://www.scala-lang.org/files/archive/scala-2.11.7.deb
+sudo dpkg -i scala-2.11.7.deb
+sudo apt-get update
+sudo apt-get install scala
 
 # spark..
 cd /ebs/apps/
-wget http://a.mbbsindia.com/spark/spark-1.6.0/spark-1.6.0-bin-hadoop2.6.tgz
-tar xvf spark-1.6.0-bin-hadoop2.6.tgz
-mv spark-1.6.0-bin-hadoop2.6 /ebs/apps/spark
+sudo wget http://a.mbbsindia.com/spark/spark-1.6.0/spark-1.6.0-bin-hadoop2.6.tgz
+sudo tar xvf spark-1.6.0-bin-hadoop2.6.tgz
+sudo mv spark-1.6.0-bin-hadoop2.6 /ebs/apps/spark
 
 
 # hadoop..
 cd /ebs/apps/ 
-wget http://www.eu.apache.org/dist/hadoop/common/hadoop-2.7.2/
-tar -xzvf hadoop-2.7.2.tar.gz
-mv hadoop-2.7.2 /ebs/apps/hadoop
+sudo wget http://www.eu.apache.org/dist/hadoop/common/hadoop-2.7.2/
+sudo tar -xzvf hadoop-2.7.2.tar.gz
+sudo mv hadoop-2.7.2 /ebs/apps/hadoop
 
-chown ubuntu -R /ebs/apps/hadoop
-mkdir -p /ebs/apps/hadoop_tmp/hdfs/namenode
-mkdir -p /ebs/apps/hadoop_tmp/hdfs/datanode
-chown ubuntu -R /ebs/apps/hadoop_tmp/
+sudo chown ubuntu -R /ebs/apps/hadoop
+sudo mkdir -p /ebs/apps/hadoop_tmp/hdfs/namenode
+sudo mkdir -p /ebs/apps/hadoop_tmp/hdfs/datanode
+sudo chown ubuntu -R /ebs/apps/hadoop_tmp/
 
 
 
@@ -62,6 +64,7 @@ echo '# -- HADOOP ENVIRONMENT VARIABLES END -- #' >> ~/.bashrc
 source ~/.bashrc
 
 cd /ebs/apps/
+sudo chmod -R 777 /ebs/apps
 
 
 # update core-site.xml
