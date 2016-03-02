25fa3a99109cd0f26efafe0b5a2e314aa684ba0e
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-03-02 22:14:04 +0530

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,10 +3,10 @@ cat > ~/.bash_profile << EOF1
 alias l='ls -al'
 
 fkill_func() {
-    PS_NAME=$1
-    for pid in $(ps -ef | grep '$PS_NAME' | awk '{print $2;}');
+    PS_NAME=\$1
+    for pid in \$(ps -ef | grep '\$PS_NAME' | awk '{print \$2;}');
     do
-        kill -9 $pid;
+        kill -9 \$pid;
     done
 }
 alias fkill=fkill_func
