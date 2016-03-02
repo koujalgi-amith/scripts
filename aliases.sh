2c7dac822c712f48ee9396aec31c160ae888b625
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-03-02 21:52:58 +0530

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,6 @@
 # Setup command aliases
 cat > ~/.bash_profile << EOF1
 alias l='ls -al'
-alias fkill='for pid in $(ps -ax | grep -i \!* | awk '{print $1;}'); do kill -9 $pid; done'
+alias pid='ps -ax | grep -i \!* | awk '{print $1;}')'
+#alias fkill='for pid in $(ps -ax | grep -i \!* | awk '{print $1;}'); do kill -9 $pid; done'
 EOF1
