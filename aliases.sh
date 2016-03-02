3f9576d6d28d15446d018651eb3d19355b62d3ed
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-03-02 22:19:52 +0530

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,12 @@ cat >> ~/.bash_profile << EOF1
 alias l='ls -al'
 
 fkill_func() {
+    if [[ \$# -eq 0 ]]; then
+	    echo "Process name not provided."
+	    echo "Usage: fkill <process-name>"
+	    return 0
+    fi
+
     PS_NAME=\$1
     for pid in \$(ps -ef | grep '\$PS_NAME' | awk '{print \$2;}');
     do
