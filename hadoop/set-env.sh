63e40d8ce1af829e6534dc191b58551f70ab5a90
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-03-07 16:11:59 +0530

    Update set-env.sh

--- hadoop/set-env.sh
+++ hadoop/set-env.sh
@@ -3,11 +3,13 @@ cat >> ~/.bash_profile << EOF0
 
 export HADOOP_HOME=<point-your-hadoop-directory>
 export HIVE_HOME=<point-your-hive-directory>
+export SPARK_HOME=<point-your-spark-directory>
 
-export PATH=\$PATH:\$HADOOP_HOME/bin:\$HIVE_HOME/bin
+export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$HIVE_HOME/bin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 
 export CLASSPATH=\$CLASSPATH:\$HADOOP_HOME/lib/*:.
 export CLASSPATH=\$CLASSPATH:\$HIVE_HOME/lib/*:.
+export CLASSPATH=\$CLASSPATH:\$SPARK_HOME/lib/*:.
 
 EOF0
 
