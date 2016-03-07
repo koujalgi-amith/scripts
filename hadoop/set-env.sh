2aea86e4a3424601ed7bd4db4a0919c47a5821a4
Amith Koujalgi <koujalgi.amith@gmail.com>
2016-03-07 16:06:21 +0530

    Update set-env.sh

--- hadoop/set-env.sh
+++ hadoop/set-env.sh
@@ -1,8 +1,8 @@
 # Setup Hadoop, Hive environment variables
 cat >> ~/.bash_profile << EOF0
 
-export HADOOP_HOME=/Users/dev/Documents/apps/hadoop-2.7.2
-export HIVE_HOME=/Users/dev/Documents/apps/apache-hive-2.0.0-bin
+export HADOOP_HOME=<point-your-hadoop-directory>
+export HIVE_HOME=<point-your-hive-directory>
 
 export PATH=\$PATH:\$HADOOP_HOME/bin:\$HIVE_HOME/bin
 
